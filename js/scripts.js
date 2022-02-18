a97e7f046463de511a394b25ae44e50063c4ad53
Ronaldo <ronaldosegundojr@usp.br>
2022-02-17 22:38:33 -0300

    att
    att

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-console.log('teste');
+$(document).ready(function() {
+
+    // Progress bar
+    let containerA = document.getElementById("circleA");
+
+    let circleA = new ProgressBar.Circle(containerA, {
+
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1400,
+        from: { color: '#AAA' },
+        to: { color: '#64daf9' },
+
+        step: function(state, circle) {
+
+            circle.path.setAttribute('stroke', state.color);
+
+            var value = Math.round(circle.value() * 60);
+
+            circle.setText(value);
+
+        }
+    });
+
+    circleA.animate(1.0);
+});
